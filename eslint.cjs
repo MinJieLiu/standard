c2e60290b26aa852603dc14bf5cd9c91d0d8f759
MinJieLiu <liumingyi_1@163.com>
2023-10-24 22:11:41 +0800

    deps: upgrade

--- eslint.cjs
+++ eslint.cjs
@@ -48,7 +48,7 @@ module.exports = {
       presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
       plugins: [
         ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        ['@babel/plugin-transform-class-properties', { loose: true }],
       ],
     },
     requireConfigFile: false,
